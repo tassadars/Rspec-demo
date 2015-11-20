5989179021176495b0bcb0cd89dd02d78ab0c031
eugen <tassadars@gmail.com>
2015-11-20 15:26:13 +0200

    Add power_down and hero info tesdt

--- hero_spec.rb
+++ hero_spec.rb
@@ -17,4 +17,16 @@ describe Hero do
 
     expect(hero.power_up).to eq 110
   end
+
+  it "can power down" do
+    hero = Hero.new 'mike'
+    expect(hero.power_down).to eq 90
+  end
+
+  it "display full hero info" do
+    hero = Hero.new 'mike'
+
+    expect(hero.hero_info).to eq 'Mike has a health of 100'
+  end
+
 end
